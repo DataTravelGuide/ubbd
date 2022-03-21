efe39573c59e4a29dab9f7d6bf3048b1bb084e08
Dongsheng <dongsheng.yang@easystack.cn>
2022-03-21 20:17:32 +0800

    tests: start fio in test_ubbdadm.sh
    Signed-off-by: Dongsheng <dongsheng.yang@easystack.cn>

--- tests/test_ubbdadm.sh
+++ tests/test_ubbdadm.sh
@@ -16,27 +16,42 @@ sh -x tests/start_ubbdd.sh 30 1 &
 sleep 2
 
 
+start_fio()
+{
+	DEV=$1
+	fio --name test --rw randwrite --bs 4K --ioengine libaio --filename $DEV  --direct 1 --numjobs 1 --iodepth 128 --time_based --runtime 100 --eta-newline 1  --rate_iops 1000 &
+}
 
-get_random()
+get_random_op()
 {
 	num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
 	op=$((num % 3))
+}
+
+get_random_id()
+{
 	num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
 	id=$((num % 10))
 }
 
 do_ubbdadm_map()
 {
-	valgrind ./ubbdadm/ubbdadm --command map --type file --filepath /dev/ram0 --filesize $((1*1024*1024*1024))
+	get_random_id
+	dev=`valgrind ./ubbdadm/ubbdadm --command map --type file --filepath /dev/ram0 --filesize $((1*1024*1024*1024))`
+	start_fio $dev
 }
 
 do_ubbdadm_unmap()
 {
+	get_random_id
 	valgrind ./ubbdadm/ubbdadm --command unmap --ubbdid $id
+	get_random_id
+	valgrind ./ubbdadm/ubbdadm --command unmap --ubbdid $id --force
 }
 
 do_ubbdadm_config()
 {
+	get_random_id
 	valgrind ./ubbdadm/ubbdadm --command config --ubbdid $id --data-pages-reserve 0
 }
 
@@ -55,14 +70,11 @@ do_ubbdadm_action()
 }
 
 while true; do
-	get_random
-	echo $op $id
+	get_random_op
 	do_ubbdadm_action
 done
 
 # cleanup
-cd $UBBD_DIR
-./ubbdadm/ubbdadm --command unmap --ubbdid 0
 sleep 3
 pkill ubbdd
 rmmod ubbd
