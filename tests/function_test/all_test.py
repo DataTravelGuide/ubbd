691ba7cebf32238728b11465c24c6d7689ce9b93
Dongsheng Yang <dongsheng.yang@easystack.cn>
2022-04-15 20:42:49 +0800

    tests: disable dry-run
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- tests/function_test/all_test.py
+++ tests/function_test/all_test.py
@@ -8,12 +8,12 @@ from avocado.core.suite import TestSuite
 ubbdadmtest_config = {'resolver.references': ['ubbdadmtest.py:Ubbdadmtest.test'],
           'yaml_to_mux.files': ['ubbdadmtest.py.data/ubbdadmtest.yaml'],
           'nrunner.max_parallel_tasks': 1,
-          'run.dry_run.enabled': True}
+          'run.dry_run.enabled': False}
 
 xfstests_config = {'resolver.references': ['xfstests.py:Xfstests.test'],
           'yaml_to_mux.files': ['xfstests.py.data/xfstests.yaml'],
           'nrunner.max_parallel_tasks': 1,
-          'run.dry_run.enabled': True}
+          'run.dry_run.enabled': False}
 
 with Job(test_suites=[TestSuite.from_config(ubbdadmtest_config, name='ubbdadmtest'),
                       TestSuite.from_config(xfstests_config, name='xfstests')]) as j:
