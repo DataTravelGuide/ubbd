80911bc34fa5f9def28f8cc204abe06876b708e8
Dongsheng Yang <dongsheng.yang@easystack.cn>
2022-05-11 15:54:40 +0800

    squash into ubbd_unittest.c
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- kmods/ubbd_unittest.c
+++ kmods/ubbd_unittest.c
@@ -1,17 +1,38 @@
 
 #include <linux/module.h>
 #include "ktf.h"
+#include "ubbd_internal.h"
 
 KTF_INIT();
 
-TEST(simple, t1)
+struct ubbd_device ubbd_dev = { 0 };
+struct ubbd_queue ubbd_q = { 0 };
+struct ubbd_sb sb = { 0 };
+
+extern struct ubbd_se *get_submit_entry(struct ubbd_queue *ubbd_q);
+TEST(ubbd_req, test_get_submit_entry)
 {
-	EXPECT_TRUE(true);
+	struct ubbd_se *se;
+
+	// empty submittion queue
+	ubbd_dev.dev_id = 0;
+	sb.cmd_head = sb.cmd_tail = 0;
+	ubbd_q.ubbd_dev = &ubbd_dev;
+	ubbd_q.sb_addr = &sb;
+	ubbd_q.cmdr = (void *)1024;
+
+	se = get_submit_entry(&ubbd_q);
+	EXPECT_ADDR_EQ(se, 1024);
+
+	// one entry
+	sb.cmd_head = CMDR_RESERVED;
+	se = get_submit_entry(&ubbd_q);
+	EXPECT_ADDR_EQ(se, 1024 + CMDR_RESERVED);
 }
 
 static void add_tests(void)
 {
-	ADD_TEST(t1);
+	ADD_TEST(test_get_submit_entry);
 }
 
 static int __init ubbd_kmod_unittest_init(void)
