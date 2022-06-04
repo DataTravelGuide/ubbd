387d0c0118acca36df0b6c7639db35479ddd3af6
Dongsheng Yang <dongsheng.yang@easystack.cn>
2022-06-04 18:34:59 +0800

    utils: add more utils for ubbd-backend
    (1) execute to run command in subprocess.
    (2) wait_for_condition to wait a condition
    (3) COMPILE_ASSERT to do some assertion in compilation stage
    
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

---
+++ lib/utils.c
@@ -0,0 +1,35 @@
+#include "utils.h"
+#include "ubbd_log.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h> 
+
+
+int execute(char* program, char** arg_list)
+{
+	pid_t pid;
+	int status;
+
+  	pid = fork();
+
+	switch(pid) {
+	case 0:
+		/* child */
+		execvp(program, arg_list);
+		ubbd_err("error execing %s : %s\n", program, strerror(errno));
+		exit(-1);
+	case -1:
+		ubbd_err("fork failed: %s\n", strerror(errno));
+		return -1;
+	default:
+		/* parent */
+		ubbd_info("start process %d to execute %s\n", pid, program);
+		wait(&status);
+		ubbd_err("status of child is : %d\n", status);
+	}
+
+	return 0;
+}
