2c020ab3ffa8f67706660b8f8ce2b3b9fe3edd69
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-02-27 10:41:14 +0000

    build_deb.sh: add codename in deb package name
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

--- build_deb.sh
+++ build_deb.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+ARCH=`dpkg --print-architecture`
+source /etc/os-release
+
 ./install_dep.sh
 git submodule update --init --recursive
+
+sed "s/@ARCH@/${ARCH}/g" debian/control.in > debian/control
+sed "s/@CODENAME@/${VERSION_CODENAME}/g" debian/changelog.in > debian/changelog
 dpkg-buildpackage -uc -us
