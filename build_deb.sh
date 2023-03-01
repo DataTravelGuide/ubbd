16a2663c283f3683dfacd1d116525a515942f017
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-03-01 06:37:36 +0000

    debian: make VERSION changable for package
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

--- build_deb.sh
+++ build_deb.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+VER=`cat VERSION`
 ARCH=`dpkg --print-architecture`
 source /etc/os-release
 
@@ -10,4 +11,5 @@ git submodule update --init --recursive
 
 sed "s/@ARCH@/${ARCH}/g" debian/control.in > debian/control
 sed "s/@CODENAME@/${VERSION_CODENAME}/g" debian/changelog.in > debian/changelog
+sed -i "s/@VERSION@/${VER}/g"  debian/changelog
 dpkg-buildpackage -uc -us
