3a960a42855f734ad8691b88d8731cff9d4c2f73
Dongsheng Yang <dongsheng.yang.linux@gmail.com>
2023-03-31 01:39:59 +0000

    install-deps.sh only
    Signed-off-by: Dongsheng Yang <dongsheng.yang.linux@gmail.com>

--- setup_ceph.sh
+++ setup_ceph.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -x
 
-git clone https://github.com/yangdongsheng/ceph.git
+apt install -y ninja-build
+git clone https://github.com/ceph/ceph
 
 cd ceph
 git checkout ubbd
 
-./run-make-check.sh  -DWITH_SPDK=OFF -DWITH_MGR=OFF -DWITH_GTEST_PARALLEL=OFF -DWITH_LIBURING=OFF -DWITH_CEPHFS=OFF -DWITH_RBD_UBBD=ON -DWITH_FIO=OFF
+./install-deps.sh
+
+
+ARGS="-DWITH_SPDK=OFF -DWITH_MGR=OFF -DWITH_GTEST_PARALLEL=OFF -DWITH_LIBURING=OFF -DWITH_CEPHFS=OFF -DWITH_RBD_UBBD=ON -DWITH_FIO=OFF" ./do_cmake.sh
+
+cd build
+ninja -j 24 install
