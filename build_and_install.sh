89192c7fb79b9dfb4a029faa1d454f913ace8625
Dongsheng Yang <dongsheng.yang@easystack.cn>
2023-06-18 11:54:54 +0000

    build_and_install.sh: add --enable-debug and --enable-asan
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- build_and_install.sh
+++ build_and_install.sh
@@ -8,7 +8,7 @@ ubbd_path=`pwd`
 ./install_dep.sh
 git submodule update --init --recursive
 
-./configure --enable-rbd-backend --enable-s3-backend --enable-cache-backend --enable-ssh-backend
+./configure --enable-rbd-backend --enable-s3-backend --enable-cache-backend --enable-ssh-backend --enable-debug --enable-asan
 # build
 make
 
