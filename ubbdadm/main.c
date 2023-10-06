aad5fdf3b33d27c55ba1d2e67772bf62ca8d5f2a
Linggang Zeng <linggang.zeng@easystack.cn>
2023-10-06 21:49:29 +0800

    Input with a json file
    Signed-off-by: Linggang Zeng <linggang.zeng@easystack.cn>

--- ubbdadm/main.c
+++ ubbdadm/main.c
@@ -6,10 +6,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <linux/limits.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+#include "cJSON.h"
 #include "libubbd.h"
 #include "ubbd_version.h"
 
@@ -75,6 +78,7 @@ static struct option const long_options[] =
 	UBBD_MAP_OPT(cache, mode)
 
 	{"help", no_argument, NULL, 'h'},
+	{"input", required_argument, NULL, 0},
 	{NULL, 0, NULL, 0},
 };
 
@@ -122,6 +126,10 @@ static void usage(int status)
 
 	/* map options */
 	printf("\n\t[map options]:\n");
+
+	print_opt_msg("input", "input map options from a file which type is JSON");
+
+	printf("\n");
 
 	print_map_opt_msg("type", "device type for mapping: file, rbd, null, mem, ssh (Experimental), cache (Experimental), s3 (Experimental)");
 	print_map_opt_msg("devsize", "size of device to map, --devsize is required except rbd and file type");
@@ -326,6 +334,85 @@ static int output_dev_info(struct ubbdd_mgmt_rsp *rsp)
 	return ret;
 }
 
+static int parse_options(const char *opt_name, struct ubbd_map_options *opts, char *optarg)
+{
+	int ret = 0;
+
+	if (!strcmp(opt_name, "dev-share-memory-size")) {
+		opts->dev_share_memory_size = atoi(optarg);
+		if (opts->dev_share_memory_size % PAGE_SIZE) {
+			printf("dev-share-memory-size: %d is not multiple of 4096.\n",
+				       opts->dev_share_memory_size);
+			return -1;
+		}
+
+		if (opts->dev_share_memory_size < 4194304) {
+			printf("dev-share-memory-size: %d is not in range of [4194304 (4M) - 1073741824 (1G)]\n",
+					opts->dev_share_memory_size);
+			return -1;
+		}
+	} else if (!strcmp(opt_name, "num-queues")) {
+		opts->num_queues = atoi(optarg);
+	} else if (!strcmp(opt_name, "read-only")) {
+		opts->read_only = true;
+	} else if (!strcmp(opt_name, "type")) {
+		opts->type = optarg;
+	} else if (!strcmp(opt_name, "cache-mode")) {
+		opts->cache_dev.cache_mode = optarg;
+	} else if (!strncmp(opt_name, "cache-dev-", 10)) {
+		ret = parse_map_options(&opts->cache_dev.cache_opts,
+				opt_name + 10,
+				optarg);
+	} else if (!strncmp(opt_name, "backing-dev-", 12)) {
+		ret = parse_map_options(&opts->cache_dev.backing_opts,
+				opt_name + 12,
+				optarg);
+	} else {
+		ret = parse_map_options(&opts->generic_dev.opts, opt_name, optarg);
+	}
+
+	return ret;
+}
+
+static int parse_input_options(struct ubbd_map_options *opts, char *optarg, cJSON **root)
+{
+	int ret = 0;
+
+	int fd = open(optarg, O_RDONLY);
+	if (fd < 0) {
+		printf("open(%s) fail\n", optarg);
+		return -1;
+	}
+
+	char buf[ARG_MAX] = {0};
+
+	int n = read(fd, buf, ARG_MAX);
+	if (n == -1) {
+		printf("read(%s) error\n", optarg);
+		return -1;
+	}
+
+	if (fd)
+		close(fd);
+
+	*root = cJSON_Parse(buf);
+	if (*root == NULL) {
+		printf("parse(%s) error\n", optarg);
+		return -1;
+	}
+
+	cJSON *index = (*root)->child;
+
+	while (index != NULL) {
+		ret = parse_options(index->string, opts, index->valuestring);
+		if (ret)
+			return -1;
+		index = index->next;
+	}
+
+	return ret;
+}
+
 int main(int argc, char **argv)
 {
 	int ch, longindex;
@@ -338,52 +425,22 @@ int main(int argc, char **argv)
 	char *restart_mode = "default";
 	struct ubbdd_mgmt_rsp rsp = { 0 };
 	struct ubbd_map_options opts = { 0 };
+	cJSON *root = NULL;
 
 	optind = 0;
 	while ((ch = getopt_long(argc, argv, short_options,
 				 long_options, &longindex)) >= 0) {
 		switch (ch) {
 		case 0:
-			if (!strcmp(long_options[longindex].name, "dev-share-memory-size")) {
-				opts.dev_share_memory_size = atoi(optarg);
-				if (opts.dev_share_memory_size % PAGE_SIZE) {
-					printf("dev-share-memory-size: %d is not multiple of 4096.\n",
-						       opts.dev_share_memory_size);
-					return -1;
-				}
-
-				if (opts.dev_share_memory_size < 4194304) {
-					printf("dev-share-memory-size: %d is not in range of [4194304 (4M) - 1073741824 (1G)]\n",
-							opts.dev_share_memory_size);
-					return -1;
-				}
-				break;
-			} else if (!strcmp(long_options[longindex].name, "num-queues")) {
-				opts.num_queues = atoi(optarg);
-				break;
-			} else if (!strcmp(long_options[longindex].name, "read-only")) {
-				opts.read_only = true;
-				break;
-			} else if (!strcmp(long_options[longindex].name, "type")) {
-				opts.type = optarg;
-				break;
-			} else if (!strcmp(long_options[longindex].name, "cache-mode")) {
-				opts.cache_dev.cache_mode = optarg;
-				break;
-			} else if (!strncmp(long_options[longindex].name, "cache-dev-", 10)) {
-				ret = parse_map_options(&opts.cache_dev.cache_opts,
-						long_options[longindex].name + 10,
-						optarg);
-			} else if (!strncmp(long_options[longindex].name, "backing-dev-", 12)) {
-				ret = parse_map_options(&opts.cache_dev.backing_opts,
-						long_options[longindex].name + 12,
-						optarg);
+			if (!strcmp(long_options[longindex].name, "input")) {
+				ret = parse_input_options(&opts, optarg, &root);
 			} else {
-				ret = parse_map_options(&opts.generic_dev.opts, long_options[longindex].name, optarg);
+				ret = parse_options(long_options[longindex].name, &opts, optarg);
 			}
 
 			if (ret) {
-				return -1;
+				ret = -1;
+				goto delete;
 			}
 			break;
 		case 'c':
@@ -405,24 +462,30 @@ int main(int argc, char **argv)
 			detach = true;
 			break;
 		case 'h':
+			if (root)
+				cJSON_Delete(root);
 			usage(0);
 		case 'v':
 			printf("ubbdadm: %s\n", UBBD_U_VERSION);
-			return 0;
+			ret = 0;
+			goto delete;
 		default:
 			printf("unrecognized ubbd option.\n");
-			return -1;
+			ret = -1;
+			goto delete;
 		}
 	}
 
 	if (argc - optind > 1) {
 		printf("Too many subcommand.\n");
-		return -1;
+		ret = -1;
+		goto delete;
 	}
 
 	if (optind >= argc) {
 		printf("Missing subcommand.\n");
-		return -1;
+		ret = -1;
+		goto delete;
 	}
 
 	command = argv[optind];
@@ -492,5 +555,9 @@ int main(int argc, char **argv)
 	}
 
 out:
+delete:
+	if (root)
+		cJSON_Delete(root);
+
 	return ret;
 }
